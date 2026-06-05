1b372aece5f6046932c1f937adbdc1c7c460fabf
Tommy0412 <37461047+Tommy0412@users.noreply.github.com>
2026-06-05 03:09:51 +0200

    Update Android.mk

--- vendor/denver/TAQ90/proprietary/vendor/Android.mk
+++ vendor/denver/TAQ90/proprietary/vendor/Android.mk
@@ -2,23 +2,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter lineage_$(TARGET_DEVICE),$(TARGET_PRODUCT)),)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := framework-res__auto_generated_rro
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := overlay/framework-res__auto_generated_rro.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/overlay
-include $(BUILD_PREBUILT)
-include $(CLEAR_VARS)
-LOCAL_MODULE := SysuiDarkThemeOverlay
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := overlay/SysuiDarkTheme/SysuiDarkThemeOverlay.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/overlay/SysuiDarkTheme
-include $(BUILD_PREBUILT)
-
 endif
